e18518e167886806de3db7a5b4473afbf57d8007
tnakamura <griefworker@gmail.com>
2025-01-08 14:45:10 +0900

    Add QueryBinder

--- src/Kuery/Query.cs
+++ src/Kuery/Query.cs
@@ -1258,7 +1258,7 @@ namespace Kuery
         }
 
         private bool IsColumnNameInUse(string name)
-            => this.columnNames.Contains(name);
+            => columnNames.Contains(name);
 
         private string GetUniqueColumnName(string name)
         {
@@ -1319,6 +1319,306 @@ namespace Kuery
                 }
                 return node;
             }
+        }
+    }
+
+    internal class QueryBinder : ExpressionVisitor
+    {
+        ColumnProjector2 columnProjector;
+        Dictionary<ParameterExpression, Expression> map;
+        int aliasCount;
+
+        internal QueryBinder()
+        {
+            columnProjector = new ColumnProjector2(CanBeColumn);
+        }
+
+        private bool CanBeColumn(Expression node)
+            => node.NodeType == (ExpressionType)DbExpressionType.Column;
+
+        internal Expression Bind(Expression node)
+        {
+            map = new Dictionary<ParameterExpression, Expression>();
+            return Visit(node);
+        }
+
+        private static Expression StripQuotes(Expression node)
+        {
+            while (node.NodeType == ExpressionType.Quote)
+            {
+                node = ((UnaryExpression)node).Operand;
+            }
+            return node;
+        }
+
+        private string GetNextAlias()
+            => $"t{aliasCount++}";
+
+        private ProjectedColumns ProjectColumns(
+            Expression node,
+            string newAlias,
+            string existingAlias)
+            => columnProjector.ProjectColumns(
+                node: node,
+                newAlias: newAlias,
+                existingAlias: existingAlias);
+
+        protected override Expression VisitMethodCall(MethodCallExpression node)
+        {
+            if (node.Method.DeclaringType == typeof(Queryable) ||
+                node.Method.DeclaringType == typeof(Enumerable))
+            {
+                switch (node.Method.Name)
+                {
+                    case nameof(Queryable.Where):
+                        return BindWhere(
+                            resultType: node.Type,
+                            source: node.Arguments[0],
+                            predicate: (LambdaExpression)StripQuotes(node.Arguments[1]));
+                    case nameof(Queryable.Select):
+                        return BindSelect(
+                            resultType: node.Type,
+                            source: node.Arguments[0],
+                            selector: (LambdaExpression)StripQuotes(node.Arguments[1]));
+                    default:
+                        throw new NotSupportedException(
+                            $"The method '{node.Method.Name}' is not supported");
+                }
+            }
+
+            return base.VisitMethodCall(node);
+        }
+
+        private Expression BindWhere(
+            Type resultType,
+            Expression source,
+            LambdaExpression predicate)
+        {
+            var projection = (ProjectionExpression)Visit(source);
+            map[predicate.Parameters[0]] = projection.Projector;
+            var where = Visit(predicate.Body);
+            var alias = GetNextAlias();
+            var pc = ProjectColumns(
+                node: projection.Projector,
+                newAlias: alias,
+                existingAlias: GetExistingAlias(projection.Source));
+            return new ProjectionExpression(
+                source: new SelectExpression(
+                    type: resultType,
+                    alias: alias,
+                    columns: pc.Columns,
+                    from: projection.Source,
+                    where: where),
+                projector: pc.Projector);
+        }
+
+        private Expression BindSelect(
+            Type resultType,
+            Expression source,
+            LambdaExpression selector)
+        {
+            var projection = (ProjectionExpression)Visit(source);
+            map[selector.Parameters[0]] = projection.Projector;
+            var expression = Visit(selector.Body);
+            var alias = GetNextAlias();
+            var pc = ProjectColumns(
+                node: expression,
+                newAlias: alias,
+                existingAlias: GetExistingAlias(projection.Source));
+            return new ProjectionExpression(
+                source: new SelectExpression(
+                    type: resultType,
+                    alias: alias,
+                    columns: pc.Columns,
+                    from: projection.Source,
+                    where: null),
+                projector: pc.Projector);
+        }
+
+        private static string GetExistingAlias(Expression source)
+        {
+            switch ((DbExpressionType)source.NodeType)
+            {
+                case DbExpressionType.Select:
+                    return ((SelectExpression)source).Alias;
+                case DbExpressionType.Table:
+                    return ((TableExpression)source).Alias;
+                default:
+                    throw new InvalidOperationException(
+                        $"Invalid source node type '{source.NodeType}'");
+            }
+        }
+
+        private bool IsTable(object value)
+        {
+            var q = value as IQueryable;
+            return q != null && q.Expression.NodeType == ExpressionType.Constant;
+        }
+
+        private string GetTableName(object table)
+        {
+            var tableQuery = (IQueryable)table;
+            var rowType = tableQuery.ElementType;
+            return rowType.Name;
+        }
+
+        private string GetColumnName(MemberInfo member)
+            => member.Name;
+
+        private Type GetColumnType(MemberInfo member)
+        {
+            var fi = member as FieldInfo;
+            if (fi != null)
+            {
+                return fi.FieldType;
+            }
+
+            var pi = (PropertyInfo)member;
+            return pi.PropertyType;
+        }
+
+        private IEnumerable<MemberInfo> GetMappedMembers(Type rowType)
+            => rowType.GetFields().Cast<MemberInfo>();
+
+        private ProjectionExpression GetTableProjection(object value)
+        {
+            var table = (IQueryable)value;
+            var tableAlias = GetNextAlias();
+            var selectAlias = GetNextAlias();
+            var bindings = new List<MemberBinding>();
+            var columns = new List<ColumnDeclaration>();
+
+            foreach (var mi in GetMappedMembers(table.ElementType))
+            {
+                var columnName = GetColumnName(mi);
+                var columnType = GetColumnType(mi);
+                var ordinal = columns.Count;
+
+                bindings.Add(
+                    Expression.Bind(
+                        member: mi,
+                        expression: new ColumnExpression(
+                            type: columnType,
+                            alias: selectAlias,
+                            name: columnName,
+                            ordinal: ordinal)));
+
+                columns.Add(
+                    new ColumnDeclaration(
+                        name: columnName,
+                        expression: new ColumnExpression(
+                            type: columnType,
+                            alias: tableAlias,
+                            name: columnName,
+                            ordinal: ordinal)));
+            }
+
+            var projector = Expression.MemberInit(
+                newExpression: Expression.New(table.ElementType),
+                bindings: bindings);
+
+            var resultType = typeof(IEnumerable<>).MakeGenericType(table.ElementType);
+
+            return new ProjectionExpression(
+                source: new SelectExpression(
+                    type: resultType,
+                    alias: selectAlias,
+                    columns: columns,
+                    from: new TableExpression(
+                        type: resultType,
+                        alias: tableAlias,
+                        name: GetTableName(table)),
+                    where: null),
+                projector: projector);
+        }
+
+        protected override Expression VisitConstant(ConstantExpression node)
+        {
+            if (IsTable(node.Value))
+            {
+                return GetTableProjection(node.Value);
+            }
+            return node;
+        }
+
+        protected override Expression VisitParameter(ParameterExpression node)
+        {
+            if (map.TryGetValue(node, out var e))
+            {
+                return e;
+            }
+            return node;
+        }
+
+        protected override Expression VisitMember(MemberExpression node)
+        {
+            var source = Visit(node.Expression);
+
+            switch (source.NodeType)
+            {
+                case ExpressionType.MemberInit:
+                    var min = (MemberInitExpression)source;
+                    for (var i = 0; i < min.Bindings.Count; i++)
+                    {
+                        var assign = min.Bindings[i] as MemberAssignment;
+                        if (assign != null && MembersMatch(assign.Member, node.Member))
+                        {
+                            return assign.Expression;
+                        }
+                    }
+                    break;
+                case ExpressionType.New:
+                    var nex = (NewExpression)source;
+                    if (nex.Members != null)
+                    {
+                        for (var i = 0; i < nex.Members.Count; i++)
+                        {
+                            if (MembersMatch(nex.Members[i], node.Member))
+                            {
+                                return nex.Arguments[i];
+                            }
+                        }
+                    }
+                    break;
+            }
+
+            if (source == node.Expression)
+            {
+                return node;
+            }
+
+            return MakeMemberAccess(source, node.Member);
+        }
+
+        private bool MembersMatch(MemberInfo a, MemberInfo b)
+        {
+            if (a == b)
+            {
+                return true;
+            }
+
+            if (a is MethodInfo && b is PropertyInfo)
+            {
+                return a == ((PropertyInfo)b).GetGetMethod();
+            }
+            else if (a is PropertyInfo && b is MethodInfo)
+            {
+                return ((PropertyInfo)a).GetGetMethod() == b;
+            }
+
+            return false;
+        }
+
+        private Expression MakeMemberAccess(Expression source, MemberInfo mi)
+        {
+            var fi = mi as FieldInfo;
+            if (fi != null)
+            {
+                return Expression.Field(source, fi);
+            }
+
+            var pi = (PropertyInfo)mi;
+            return Expression.Property(source, pi);
         }
     }
 }
