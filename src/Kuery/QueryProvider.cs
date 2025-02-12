4211ef813f3db751ac735db077702d0d41a59e58
tnakamura <griefworker@gmail.com>
2025-02-12 17:07:10 +0900

    Add Query<T> and QueryProvider

---
+++ src/Kuery/QueryProvider.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace Kuery
+{
+    internal class Query<T> : IQueryable<T>, IOrderedQueryable<T>
+    {
+        private readonly QueryProvider provider;
+
+        private readonly Expression expression;
+
+        internal Query(QueryProvider provider)
+        {
+            if (provider == null)
+            {
+                throw new ArgumentNullException(nameof(provider));
+            }
+
+            this.provider = provider;
+            expression = Expression.Constant(this);
+        }
+
+        internal Query(QueryProvider provider, Expression expression)
+        {
+            if (provider == null)
+            {
+                throw new ArgumentNullException(nameof(provider));
+            }
+            if (expression == null)
+            {
+                throw new ArgumentNullException(nameof(expression));
+            }
+            if (!typeof(IQueryable<T>).IsAssignableFrom(expression.Type))
+            {
+                throw new ArgumentOutOfRangeException(nameof(expression));
+            }
+
+            this.provider = provider;
+            this.expression = expression;
+        }
+
+        /// <inheritdoc/>
+        public Type ElementType => typeof(T);
+
+        /// <inheritdoc/>
+        public Expression Expression => expression;
+
+        /// <inheritdoc/>
+        public IQueryProvider Provider => provider;
+
+        /// <inheritdoc/>
+        public IEnumerator<T> GetEnumerator()
+        {
+            return ((IEnumerable<T>)provider.Execute<T>(expression)).GetEnumerator();
+        }
+
+        /// <inheritdoc/>
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable)provider.Execute(expression)).GetEnumerator();
+        }
+
+        /// <inheritdoc/>
+        public override string ToString()
+        {
+            return provider.GetQueryText(expression);
+        }
+    }
+
+    internal abstract class QueryProvider : IQueryProvider
+    {
+        private readonly DbConnection connection;
+
+        protected internal QueryProvider(DbConnection connection)
+        {
+            this.connection = connection;
+        }
+
+        /// <inheritdoc/>
+        public IQueryable CreateQuery(Expression expression)
+        {
+            var elementType = TypeSystem.GetElementType(expression.Type);
+            var queryType = typeof(Query<>).MakeGenericType(elementType);
+            try
+            {
+                return (IQueryable)Activator.CreateInstance(queryType, this, expression);
+            }
+            catch (TargetInvocationException ex)
+            {
+                throw ex.InnerException;
+            }
+        }
+
+        /// <inheritdoc/>
+        public IQueryable<TElement> CreateQuery<TElement>(Expression expression)
+        {
+            return new Query<TElement>(this, expression);
+        }
+
+        /// <inheritdoc/>
+        public abstract object Execute(Expression expression);
+
+        /// <inheritdoc/>
+        public TResult Execute<TResult>(Expression expression)
+        {
+            return (TResult)Execute(expression);
+        }
+
+        public abstract string GetQueryText(Expression expression);
+    }
+
+    internal class DbQueryProvider : QueryProvider
+    {
+        internal DbQueryProvider(DbConnection connection) : base(connection)
+        {
+        }
+
+        /// <inheritdoc/>
+        public override object Execute(Expression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc/>
+        public override string GetQueryText(Expression expression)
+        {
+            return expression.ToString();
+        }
+    }
+
+    internal static class TypeSystem
+    {
+        internal static Type GetElementType(Type sequenceType)
+        {
+            var ienumerableType = FindIEnumerable(sequenceType);
+            if (ienumerableType == null)
+            {
+                return sequenceType;
+            }
+            return ienumerableType.GetGenericArguments()[0];
+        }
+
+        private static Type FindIEnumerable(Type sequenceType)
+        {
+            if (sequenceType == null ||
+                sequenceType == typeof(string))
+            {
+                return null;
+            }
+
+            if (sequenceType.IsArray)
+            {
+                return typeof(IEnumerable<>).MakeGenericType(sequenceType.GetElementType());
+            }
+
+            if (sequenceType.IsGenericType)
+            {
+                foreach (var arg in sequenceType.GetGenericArguments())
+                {
+                    var ienumerableType = typeof(IEnumerable<>).MakeGenericType(arg);
+                    if (ienumerableType.IsAssignableFrom(sequenceType))
+                    {
+                        return ienumerableType;
+                    }
+                }
+            }
+
+            var interfaceTypes = sequenceType.GetInterfaces();
+            if (interfaceTypes != null && interfaceTypes.Length > 0)
+            {
+                foreach (var interfaceType in interfaceTypes)
+                {
+                    var ienumerableType = FindIEnumerable(interfaceType);
+                    if (ienumerableType != null)
+                    {
+                        return ienumerableType;
+                    }
+                }
+            }
+
+            if (sequenceType.BaseType != null &&
+                sequenceType.BaseType != typeof(object))
+            {
+                return FindIEnumerable(sequenceType.BaseType);
+            }
+
+            return null;
+        }
+    }
+}
---
+++ test/Kuery.Tests/Sqlite/QueryProviderTest.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Data.Common;
+using System.Linq;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Kuery.Tests.Sqlite
+{
+    public class QueryProviderTest : IClassFixture<SqliteFixture>, IDisposable
+    {
+        [Table("books")]
+        class Book
+        {
+            [PrimaryKey]
+            [Column("id")]
+            public string Id { get; set; }
+
+            [NotNull]
+            [Column("code")]
+            public string Code { get; set; }
+
+            [NotNull]
+            [Column("title")]
+            public string Title { get; set; }
+
+            [NotNull]
+            [Column("summary")]
+            public string Summary { get; set; }
+
+            [NotNull]
+            [Column("price")]
+            public decimal Price { get; set; }
+        }
+
+        private readonly SqliteFixture fixture;
+
+        private readonly DbConnection connection;
+
+        private readonly ITestOutputHelper output;
+
+        private readonly DbQueryProvider provider;
+
+        public QueryProviderTest(ITestOutputHelper output, SqliteFixture fixture)
+        {
+            this.output = output;
+            this.fixture = fixture;
+            connection = fixture.CreateConnection();
+            provider = new DbQueryProvider(connection);
+        }
+
+        public void Dispose()
+        {
+            connection.Dispose();
+        }
+
+        [Fact]
+        public void WhereTest()
+        {
+            var query = new Query<Book>(provider)
+                .Where(x => x.Price >= 1000);
+
+            output.WriteLine(query.Expression.ToString());
+        }
+
+        [Fact]
+        public void WhereOrderByTest()
+        {
+            var query = new Query<Book>(provider)
+                .Where(x => x.Price >= 1000)
+                .OrderBy(x => x.Code);
+
+            output.WriteLine(query.Expression.ToString());
+        }
+    }
+}
