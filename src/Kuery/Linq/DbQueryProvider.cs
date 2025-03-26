37147b39539cb1e500fb781d58ce2e4818d820b2
tnakamura <griefworker@gmail.com>
2025-03-26 17:08:41 +0900

    Implemented Select

--- src/Kuery/Linq/DbQueryProvider.cs
+++ src/Kuery/Linq/DbQueryProvider.cs
@@ -17,32 +17,50 @@ namespace Kuery.Linq
         /// <inheritdoc/>
         internal override string GetQueryText(Expression expression)
         {
-            return Translate(expression);
+            return Translate(expression).CommandText;
         }
 
         /// <inheritdoc/>
         internal override object Execute(Expression expression)
         {
+            var result = Translate(expression);
             var cmd = connection.CreateCommand();
-            cmd.CommandText = Translate(expression);
+            cmd.CommandText = result.CommandText;
             var reader = cmd.ExecuteReader();
             var elementType = TypeSystem.GetElementType(expression.Type);
 
-            return Activator.CreateInstance(
-                type: typeof(ObjectReader<>).MakeGenericType(elementType),
-                bindingAttr: BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic,
-                binder: null,
-                args: new object[]
-                {
-                    reader,
-                },
-                culture: null);
+            if (result.Projector != null)
+            {
+                var projector = result.Projector.Compile();
+                return Activator.CreateInstance(
+                    type: typeof(ProjectionReader<>).MakeGenericType(elementType),
+                    bindingAttr: BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic,
+                    binder: null,
+                    args: new object[]
+                    {
+                        reader,
+                        projector,
+                    },
+                    culture: null);
+            }
+            else
+            {
+                return Activator.CreateInstance(
+                    type: typeof(ObjectReader<>).MakeGenericType(elementType),
+                    bindingAttr: BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic,
+                    binder: null,
+                    args: new object[]
+                    {
+                        reader,
+                    },
+                    culture: null);
+            }
         }
 
-        private string Translate(Expression expression)
+        private TranslateResult Translate(Expression expression)
         {
             expression = Evaluator.PartialEval(expression);
-            return new QueryTranslator().Translate(expression).CommandText;
+            return new QueryTranslator().Translate(expression);
         }
     }
 }
