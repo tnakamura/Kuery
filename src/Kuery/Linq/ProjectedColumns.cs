41a3955f9a748e5237fba2eb283cee5c026b6eb5
tnakamura <griefworker@gmail.com>
2025-04-03 16:56:38 +0900

    Added ProjectedColumns

---
+++ src/Kuery/Linq/ProjectedColumns.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.ObjectModel;
+using System.Linq.Expressions;
+using Kuery.Linq.Expressions;
+
+namespace Kuery.Linq
+{
+    internal sealed class ProjectedColumns
+    {
+        internal ProjectedColumns(
+            Expression projection,
+            ReadOnlyCollection<ColumnDeclaration> columns)
+        {
+            Projection = projection;
+            Columns = columns;
+        }
+
+        internal Expression Projection { get; }
+
+        internal ReadOnlyCollection<ColumnDeclaration> Columns { get; }
+    }
+}
